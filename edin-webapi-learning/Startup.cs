1bfd4c8a65abf197a81b681184becc88cb7d62a0
Edin Agic <edin.agic@tacta.io>
2022-09-03 18:05:17 +0200

    [edin] Swagger added

--- edin-webapi-learning/Startup.cs
+++ edin-webapi-learning/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace edin_webapi_learning
 {
@@ -25,12 +26,24 @@ namespace edin_webapi_learning
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "edin-webapi-learning", Version = "v1" });
+            });
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "edin-webapi-learning V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
